0923661ceae8553dbab37224c9c6b56f1b5f4ee2
jobaer-ahmed <ajobaer08@gmail.com>
2022-08-17 17:06:27 +0800

    validation complete

--- js/withdraw.js
+++ js/withdraw.js
@@ -4,25 +4,39 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     newWithdrawAmount = parseFloat(newWithdrawAmountString);
 
 
+    withdrawField.value = '';
+
+    if (isNaN(newWithdrawAmount)) {
+        alert('Please provide a valid number!')
+        return;
+    }
+
+
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 
 
-    const currenWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-
-
-    withdrawTotalElement.innerText = currenWithdrawTotal;
 
 
     const balaceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balaceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
+
+    if (newWithdrawAmount > previousBalanceTotal) {
+        alert('Fuck you!!!!!!');
+        return;
+    }
+
+    const currenWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currenWithdrawTotal;
+
+
     const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
 
     balaceTotalElement.innerText = newBalanceTotal
 
 
-    withdrawField.value = '';
+
 })
